defa173b6f35ea023f9b20afff260832fbe6a04f
Drew Rotheram <drew.rotheram@gmail.coom>
2020-07-28 21:52:39 -0700

    add missing gmf fields

--- scripts/Create_table_s_sitemesh_update.sql
+++ scripts/Create_table_s_sitemesh_update.sql
@@ -1,5 +1,11 @@
 -- update gmf data and sitemesh data into 1 table incorporating assetID
 
+ALTER TABLE gmf.s_gmfdata_{eqScenario} ADD COLUMN IF NOT EXISTS "gmv_pgv" float DEFAULT 0;
+ALTER TABLE gmf.s_gmfdata_{eqScenario} ADD COLUMN IF NOT EXISTS "gmv_SA(0.1)" float DEFAULT 0;
+ALTER TABLE gmf.s_gmfdata_{eqScenario} ADD COLUMN IF NOT EXISTS "gmv_SA(0.2)" float DEFAULT 0;
+ALTER TABLE gmf.s_gmfdata_{eqScenario} ADD COLUMN IF NOT EXISTS "gmv_SA(2.0)" float DEFAULT 0;
+
+
 DROP TABLE IF EXISTS gmf.gmfdata_sitemesh_{eqScenario}, gmf.gmfdata_sitemesh_{eqScenario}_xref;
 
 SELECT 
