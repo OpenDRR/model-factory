016956f987a5ada7921375b476c8311d8770a7ff
Drew Rotheram <drew.rotheram@gmail.coom>
2020-09-29 14:52:07 -0700

    Additional fix to boundaries reference

--- scripts/Update_boundaries_SAUID_table.sql
+++ scripts/Update_boundaries_SAUID_table.sql
@@ -1,9 +1,9 @@
 -- script to add geometry point field
 
 -- add geometries field to enable PostGIS (WGS1984 SRID = 4326)
-ALTER TABLE boundaries."Geometry SAUID" ADD COLUMN geompoint geometry(Point,4326);
-UPDATE boundaries."Geometry SAUID" SET geompoint = st_setsrid(st_makepoint("Lon","Lat"),4326);
+ALTER TABLE boundaries."Geometry_SAUID" ADD COLUMN geompoint geometry(Point,4326);
+UPDATE boundaries."Geometry_SAUID" SET geompoint = st_setsrid(st_makepoint("Lon","Lat"),4326);
 
 -- fix error geometries
-UPDATE boundaries."Geometry SAUID"
+UPDATE boundaries."Geometry_SAUID"
 SET geom = ST_MakeValid(geom);
