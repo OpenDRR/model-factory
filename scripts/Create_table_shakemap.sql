fccf01b6eb6d75777e2dd5f8aaa6640f756ce400
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-10-29 11:17:52 -0700

    Update Create_table_shakemap.sql

--- scripts/Create_table_shakemap.sql
+++ scripts/Create_table_shakemap.sql
@@ -18,7 +18,7 @@ CREATE TABLE gmf.shakemap_{eqScenario}(
 );
 
 -- import exposure from csv
-COPY gmf.shakemap_acm7p2_lrdmf (site_id,gmv_PGA,"gmv_SA(0.1)","gmv_SA(0.2)","gmv_SA(0.3)","gmv_SA(0.5)","gmv_SA(0.6)","gmv_SA(1.0)","gmv_SA(2.0)",lon,lat)
+COPY gmf.shakemap_{eqScenario} (site_id,gmv_PGA,"gmv_SA(0.1)","gmv_SA(0.2)","gmv_SA(0.3)","gmv_SA(0.5)","gmv_SA(0.6)","gmv_SA(1.0)","gmv_SA(2.0)",lon,lat)
     FROM '/usr/src/app/s_shakemap_{eqScenario}_234.txt'
     --FROM '/usr/src/app/s_shakemap_ACM7p2_LRDMF_234.txt'
         WITH 
@@ -26,9 +26,9 @@ COPY gmf.shakemap_acm7p2_lrdmf (site_id,gmv_PGA,"gmv_SA(0.1)","gmv_SA(0.2)","gmv
           CSV HEADER ;
 
 -- add geometries field to enable PostGIS (WGS1984 SRID = 4326)
-ALTER TABLE gmf.shakemap_acm7p2_lrdmf ADD COLUMN geom geometry(Point,4326);
-UPDATE gmf.shakemap_acm7p2_lrdmf SET geom = st_setsrid(st_makepoint(lon,lat),4326);
+ALTER TABLE gmf.shakemap_{eqScenario} ADD COLUMN geom geometry(Point,4326);
+UPDATE gmf.shakemap_{eqScenario} SET geom = st_setsrid(st_makepoint(lon,lat),4326);
 
 -- create spatial index
-CREATE INDEX shakemap_acm7p2_lrdmf_idx
-ON gmf.shakemap_acm7p2_lrdmf using GIST (geom);
+CREATE INDEX shakemap_{eqScenario}_idx
+ON gmf.shakemap_{eqScenario} using GIST (geom);
