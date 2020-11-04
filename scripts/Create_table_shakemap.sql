6f975e755fb3042453661ffa7712a0375512bd00
wkhchow <59062243+wkhchow@users.noreply.github.com>
2020-11-03 16:25:01 -0800

    Update Create_table_shakemap.sql

--- scripts/Create_table_shakemap.sql
+++ scripts/Create_table_shakemap.sql
@@ -1,8 +1,9 @@
 -- script to generate shakemap table
-DROP TABLE IF EXISTS gmf.shakemap_{eqScenario};
+DROP TABLE IF EXISTS gmf.shakemap_{eqScenario} CASCADE;
 
 -- create table
-CREATE TABLE gmf.shakemap_{eqScenario}(
+CREATE TABLE gmf.shakemap_{eqScenario}
+(
     site_id varchar,
     gmv_PGA float,
     "gmv_SA(0.1)" float,
@@ -25,6 +26,7 @@ COPY gmf.shakemap_{eqScenario} (site_id,gmv_PGA,"gmv_SA(0.1)","gmv_SA(0.2)","gmv
           DELIMITER AS ','
           CSV HEADER ;
 
+/*
 -- add geometries field to enable PostGIS (WGS1984 SRID = 4326)
 ALTER TABLE gmf.shakemap_{eqScenario} ADD COLUMN geom geometry(Point,4326);
 UPDATE gmf.shakemap_{eqScenario} SET geom = st_setsrid(st_makepoint(lon,lat),4326);
@@ -32,3 +34,4 @@ UPDATE gmf.shakemap_{eqScenario} SET geom = st_setsrid(st_makepoint(lon,lat),432
 -- create spatial index
 CREATE INDEX shakemap_{eqScenario}_idx
 ON gmf.shakemap_{eqScenario} using GIST (geom);
+*/
