6c88b744d60a1419f73371d120da035900828c45
Drew Rotheram <drew.rotheram@gmail.coom>
2020-11-30 18:04:09 -0800

    Minor SQL fixes

--- scripts/Create_table_vs_30_CAN_site_model_xref.sql
+++ scripts/Create_table_vs_30_CAN_site_model_xref.sql
@@ -18,7 +18,7 @@ CREATE TABLE vs30.vs30_can_site_model_xref(
 
 -- import exposure from csv
 COPY vs30.vs30_can_site_model_xref (id, asset_lon, asset_lat, vs30, z1pt0, z2pt5, vs_lon, vs_lat, distance)
-    FROM 'D:\Workspace\data\source datasets\dsra\vs30_CAN_site_model_xref.csv'
+    FROM '/usr/src/app/vs30_CAN_site_model_xref.csv'
         WITH 
           DELIMITER AS ','
           CSV HEADER ;
