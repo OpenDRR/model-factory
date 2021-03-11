92619d8035daf81f8cb5317c6a09955ae33b7b80
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-03-10 16:40:23 -0800

    update to reflect additional columns in newest source table

--- scripts/Create_table_mh_intensity_canada_v2.sql
+++ scripts/Create_table_mh_intensity_canada_v2.sql
@@ -4,34 +4,43 @@ DROP TABLE IF EXISTS mh.mh_intensity_canada CASCADE;
 
 -- create table
 CREATE TABLE mh.mh_intensity_canada(
-    PRIMARY KEY(sauidt),
-    sauidt varchar,
-    lon float,
-    lat float,
-    pgv2500 float,
-    pgv500 float,
-    pga2500 float,
-    pga500 float,
-    vs30 float,
-    mmi6 float,
-    mmi7 float,
-    mmi8 float,
-    tsun500 float,
-    fld200 float,
-    fld500 float,
-    fld1000 float,
-    wildfire float,
-    lndsus float,
-    cy100 float,
-    cy250 float,
-    cy500 float,
-    cy1000 float
+PRIMARY KEY(sauidt),
+sauidt varchar,
+sauidi integer,
+lon float,
+lat float,
+pgv2500 float,
+pgv500 float,
+pga2500 float,
+pga500 float,
+vs30 float,
+mmi6 float,
+mmi7 float,
+mmi8 float,
+tsun500 float,
+fld50_jrc float,
+fld100_jrc float,
+fld200_jrc float,
+fld500_jrc float,
+fld200_unep float,
+fld500_unep float,
+fld1000_unep float,
+wildfire float,
+wui_type varchar,
+lndsus float,
+cy100 float,
+cy250 float,
+cy500 float,
+cy1000 float,
+svlt_score integer
+
 );
 
 /*
 -- import exposure from csv
-COPY mh.mh_intensity_canada(sauidt,lon,lat,pgv2500,pgv500,pga2500,pga500,vs30,mmi6,mmi7,mmi8,tsun500,fld200,fld500,fld1000,wildfire,lndsus,cy100,cy250,cy500,cy1000)
-    FROM 'D:\Workspace\data\source datasets\mh\hazard-intensity_sauid.csv'
+COPY mh.mh_intensity_canada(sauidt,sauidi,lon,lat,pgv2500,pgv500,pga2500,pga500,vs30,mmi6,mmi7,mmi8,tsun500,fld50_jrc,fld100_jrc,fld200_jrc,fld500_jrc,fld200_unep,fld500_unep,fld1000_unep,wildfire,wui_type,lndsus,cy100,cy250,
+cy500,cy1000,svlt_score)
+    FROM 'D:\Workspace\data\source datasets\mh\HTi_sauid_2021.csv'
         WITH 
           DELIMITER AS ','
           CSV HEADER ;
