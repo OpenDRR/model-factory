e906d20619241f33325ba736bd1d8dde6f19d3e6
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-05-26 16:48:53 -0700

    Update Create_table_sovi_thresholds.sql

--- scripts/Create_table_sovi_thresholds.sql
+++ scripts/Create_table_sovi_thresholds.sql
@@ -36,11 +36,3 @@ pubtrans_t float,
 retail_t float
 
 );
-
-/*
--- import exposure from csv
-COPY sovi.sovi_thresholds(sactype,pop_ha_t,bus_ha_t,pre_1975_t,hshld_nsuit_t,hshld_mntnage_t,hshld_mntn1_t,renter_t,live_alone_t,lonepar3kids_t,fam_gt5_t,moved_lt1_t,imm_lt5_t,nowrkplace_t,no_engfr_t,nosec_school_t,age_gt65_t,age_lt6_t,indigenous_t,vis_min_t,shltr_gt30_t,
-inc_lowdecile_t,unemployed_t,work_none_t,work_parttime_t,employ_inc_t,inc_indv_t,inc_hshld_t,agemedian_t,health_t,pubtrans_t,retail_t)
-    FROM '/usr/src/app/sovi_thresholds_2021.csv'
-          WITH DELIMITER AS ',' CSV HEADER ;
-*/
