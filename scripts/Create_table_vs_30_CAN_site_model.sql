a59a0d4254a6b31d57416988579af9f6c46538c5
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-05-26 13:51:28 -0700

    header change ( remove objectid)

--- scripts/Create_table_vs_30_CAN_site_model.sql
+++ scripts/Create_table_vs_30_CAN_site_model.sql
@@ -5,37 +5,9 @@ DROP TABLE IF EXISTS vs30.vs30_can_site_model;
 
 -- create table
 CREATE TABLE vs30.vs30_can_site_model(
-    objectid varchar,
     lon float,
     lat float,
     vs30 float,
     z1pt0 float,
-    z2pt5 float
-    
+    z2pt5 float   
 );
-
-/*
--- import exposure from csv
-COPY vs30.vs30_can_site_model (objectid, lon, lat, vs30, z1pt0, z2pt5)
-    FROM '/usr/src/app/site-vgrid_CA.csv'
-        WITH 
-          DELIMITER AS ','
-          CSV HEADER ;
-
-
--- add geometries field to enable PostGIS (WGS1984 SRID = 4326)
-ALTER TABLE vs30.vs30_can_site_model ADD COLUMN geom geometry(Point,4326);
-UPDATE vs30.vs30_can_site_model SET geom = st_setsrid(st_makepoint(lon,lat),4326);
-
--- create spatial index
-CREATE INDEX vs30_can_site_model_idx
-ON vs30.vs30_can_site_model using GIST (geom);
-
-
--- add missing columns that does not exist in source sample data but should be included in future datasets.
---ALTER TABLE vs30.vs30_can_site_model ADD COLUMN IF NOT EXISTS z1pt0 float DEFAULT 0;
---ALTER TABLE vs30.vs30_can_site_model ADD COLUMN IF NOT EXISTS z2pt5 float DEFAULT 0;
-
-ALTER TABLE vs30.vs30_can_site_model ADD COLUMN IF NOT EXISTS z1pt0 float DEFAULT NULL;
-ALTER TABLE vs30.vs30_can_site_model ADD COLUMN IF NOT EXISTS z2pt5 float DEFAULT NULL;
-*/
