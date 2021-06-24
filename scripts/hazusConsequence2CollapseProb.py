cd252f0cbff64f717d75bb3a6bc1ce1ccdc152be
Damon <damonulmi@gmail.com>
2021-06-24 12:29:07 -0700

    Added scripts/hazusConsequence2CollapseProb.py

---
+++ scripts/hazusConsequence2CollapseProb.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import openpyxl
+
+
+def main():
+    df = pd.read_excel('Hazus_Consequence_Parameters.xlsx',
+                       engine='openpyxl',  # Necessary for .xlsx
+                       sheet_name='Collapse Rates',
+                       skiprows=2, header=None,  # Drop the first 2 rows,
+                       names=['eqbldgtype', 'collapse_pc'])  # Add column names
+
+    # Convert percentages to decimal
+    df['collapse_pc'] /= 100
+
+    # Convert to .csv and add quotes around all entries
+    df.to_csv('collapse_probability.csv', index=False,
+              quoting=1)
+
+
+if __name__ == '__main__':
+    main()
