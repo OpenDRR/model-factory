fbbcefabcf137a0cf283c5905c871272f5564f6b
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-10-05 16:42:16 -0700

    Update psra_2.Create_table_updates.sql
    - removed commented out codes
    - renamed r2 to r1
    - add occupants, removed trt

--- scripts/psra_2.Create_table_updates.sql
+++ scripts/psra_2.Create_table_updates.sql
@@ -7,82 +7,10 @@ UPDATE psra_{prov}.psra_{prov}_hmaps SET geom = st_setsrid(st_makepoint(lon,lat)
 
 -- create index
 CREATE INDEX IF NOT EXISTS {prov}_hmaps_idx ON psra_{prov}.psra_{prov}_hmaps using GIST(geom);
-
-
-/*
--- attach assetID to hazard mean map based on closest location, comment out section after processed xrefs are pushed on github
-DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_hmaps_xref CASCADE;
-
-CREATE TABLE psra_{prov}.psra_{prov}_hmaps_xref AS
-SELECT
-a.id,
-a.sauid,
-a.sauidlon AS "asset_lon",
-a.sauidlat AS "asset_lat",
-b.lon,
-b.lat,
-ST_Distance(a.geom,b.geom) AS "distance",
-b."PGA_0.02",
-b."PGA_0.1",
-b."SA(0.1)_0.02",
-b."SA(0.1)_0.1",
-b."SA(0.2)_0.02",
-b."SA(0.2)_0.1",
-b."SA(0.3)_0.02",
-b."SA(0.3)_0.1",
-b."SA(0.5)_0.02",
-b."SA(0.5)_0.1",
-b."SA(0.6)_0.02",
-b."SA(0.6)_0.1",
-b."SA(1.0)_0.02",
-b."SA(1.0)_0.1",
-b."SA(10.0)_0.02",
-b."SA(10.0)_0.1",
-b."SA(2.0)_0.02",
-b."SA(2.0)_0.1",
-b."SA(5.0)_0.02",
-b."SA(5.0)_0.1"
-
---FROM psra_{prov}.exposure_{prov} a
-FROM exposure.canada_exposure a
-CROSS JOIN LATERAL 
-(
-SELECT 
-lon,
-lat,
-"PGA_0.02",
-"PGA_0.1",
-"SA(0.1)_0.02",
-"SA(0.1)_0.1",
-"SA(0.2)_0.02",
-"SA(0.2)_0.1",
-"SA(0.3)_0.02",
-"SA(0.3)_0.1",
-"SA(0.5)_0.02",
-"SA(0.5)_0.1",
-"SA(0.6)_0.02",
-"SA(0.6)_0.1",
-"SA(1.0)_0.02",
-"SA(1.0)_0.1",
-"SA(10.0)_0.02",
-"SA(10.0)_0.1",
-"SA(2.0)_0.02",
-"SA(2.0)_0.1",
-"SA(5.0)_0.02",
-"SA(5.0)_0.1",
-geom
-
-FROM psra_{prov}.psra_{prov}_hmaps
-ORDER BY a.geom <-> geom
-LIMIT 1
-) AS b;
-*/
-
--- create index
 CREATE INDEX IF NOT EXISTS psra_{prov}_hmaps_xref_id_idx ON psra_{prov}.psra_{prov}_hmaps_xref(id);
 
-DROP TABLE IF EXISTS psra_{prov}.exposure_{prov};
 
+DROP TABLE IF EXISTS psra_{prov}.exposure_{prov};
 
 /* psra_2.Create_table_dmg_mean.sql */
 -- combine cd b0 and r2 table
@@ -133,7 +61,7 @@ ALTER TABLE psra_{prov}.psra_{prov}_cd_dmg_mean ADD PRIMARY KEY (asset_id);
 
 
 
--- combine ed b0 and r2 table
+-- combine ed b0 and r1 table
 CREATE TABLE psra_{prov}.psra_{prov}_ed_dmg_mean AS
 (SELECT
 a.asset_id,
@@ -167,14 +95,14 @@ a.structural_slight AS "structural_slight_b0",
 a.structural_moderate AS "structural_moderate_b0",
 a.structural_extensive AS "structural_extensive_b0",
 a.structural_complete AS "structural_complete_b0",
-b.structural_no_damage AS "structural_no_damage_r2",
-b.structural_slight AS "structural_slight_r2",
-b.structural_moderate AS "structural_moderate_r2",
-b.structural_extensive AS "structural_extensive_r2",
-b.structural_complete AS "structural_complete_r2"
+b.structural_no_damage AS "structural_no_damage_r1",
+b.structural_slight AS "structural_slight_r1",
+b.structural_moderate AS "structural_moderate_r1",
+b.structural_extensive AS "structural_extensive_r1",
+b.structural_complete AS "structural_complete_r1"
 
 FROM psra_{prov}.psra_{prov}_ed_dmg_mean_b0 a
-INNER JOIN psra_{prov}.psra_{prov}_ed_dmg_mean_r2 b ON a.asset_id = b.asset_id
+INNER JOIN psra_{prov}.psra_{prov}_ed_dmg_mean_r1 b ON a.asset_id = b.asset_id
 );
 
 ALTER TABLE psra_{prov}.psra_{prov}_ed_dmg_mean ADD PRIMARY KEY (asset_id);
@@ -182,35 +110,34 @@ ALTER TABLE psra_{prov}.psra_{prov}_ed_dmg_mean ADD PRIMARY KEY (asset_id);
 -- create index
 CREATE INDEX IF NOT EXISTS psra_{prov}_ed_dmg_mean_asset_id_idx ON psra_{prov}.psra_{prov}_ed_dmg_mean(asset_id);
 
-DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_cd_dmg_mean_b0, psra_{prov}.psra_{prov}_cd_dmg_mean_r2, psra_{prov}.psra_{prov}_ed_dmg_mean_b0, psra_{prov}.psra_{prov}_ed_dmg_mean_r2 CASCADE;
+DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_cd_dmg_mean_b0, psra_{prov}.psra_{prov}_cd_dmg_mean_r2, psra_{prov}.psra_{prov}_ed_dmg_mean_b0, psra_{prov}.psra_{prov}_ed_dmg_mean_r1 CASCADE;
 
 
 
 /* psra_3.Create_table_agg_curves_stats.sql */
--- combine b0 and r2 tables
+-- combine b0 and r1 tables
 CREATE TABLE psra_{prov}.psra_{prov}_agg_curves_stats AS
 (SELECT
 a.return_period,
-a.stat,
 a.loss_type,
 a.fsauid,
 a."GenOcc",
 a."GenType",
 a.loss_value AS "loss_value_b0",
 a.loss_ratio AS "loss_ratio_b0",
-b.loss_value AS "loss_value_r2",
-b.loss_ratio AS "loss_ratio_r2",
+b.loss_value AS "loss_value_r1",
+b.loss_ratio AS "loss_ratio_r1",
 a.annual_frequency_of_exceedence
 FROM psra_{prov}.psra_{prov}_agg_curves_stats_b0 a
-LEFT JOIN psra_{prov}.psra_{prov}_agg_curves_stats_r2 b ON a.return_period = b.return_period AND a.stat = b.stat AND a.loss_type = b.loss_type AND a.fsauid = b.fsauid AND a."GenOcc" = b."GenOcc" AND
+LEFT JOIN psra_{prov}.psra_{prov}_agg_curves_stats_r1 b ON a.return_period = b.return_period AND a.loss_type = b.loss_type AND a.fsauid = b.fsauid AND a."GenOcc" = b."GenOcc" AND
 a."GenType" = b."GenType" and a.annual_frequency_of_exceedence = b.annual_frequency_of_exceedence);
 
-DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_agg_curves_stats_b0, psra_{prov}.psra_{prov}_agg_curves_stats_r2;
+DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_agg_curves_stats_b0, psra_{prov}.psra_{prov}_agg_curves_stats_r1;
 
 
 
 /* psra_3.Create_table_avg_losses_stats.sql */
--- combine b0 and r2 table
+-- combine b0 and r1 table
 CREATE TABLE psra_{prov}.psra_{prov}_avg_losses_stats AS
 (SELECT
 a.asset_id,
@@ -241,18 +168,20 @@ a.lon,
 a.lat,
 a.contents AS "contents_b0",
 a.nonstructural AS "nonstructural_b0",
+a.occupants AS "occupants_b0",
 a.structural AS "structural_b0",
-b.contents AS "contents_r2",
-b.nonstructural AS "nonstructural_r2",
-b.structural AS "structural_r2"
+b.contents AS "contents_r1",
+b.nonstructural AS "nonstructural_r1",
+b.occupants AS "occupants_r1",
+b.structural AS "structural_r1"
 
 FROM psra_{prov}.psra_{prov}_avg_losses_stats_b0 a
-INNER JOIN psra_{prov}.psra_{prov}_avg_losses_stats_r2 b ON a.asset_id = b.asset_id);
+INNER JOIN psra_{prov}.psra_{prov}_avg_losses_stats_r1 b ON a.asset_id = b.asset_id);
 
 -- create index
 CREATE INDEX IF NOT EXISTS psra_{prov}_avg_losses_stats_asset_id_idx ON psra_{prov}.psra_{prov}_avg_losses_stats(asset_id);
 
-DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_avg_losses_stats_b0, psra_{prov}.psra_{prov}_avg_losses_stats_r2;
+DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_avg_losses_stats_b0, psra_{prov}.psra_{prov}_avg_losses_stats_r1;
 
 
 
@@ -263,39 +192,36 @@ CREATE TABLE psra_{prov}.psra_{prov}_src_loss_b0_temp AS
 SELECT
 source,
 loss_type,
-trt,
 SUM(loss_value) AS "loss_value",
 region
 
 FROM psra_{prov}.psra_{prov}_src_loss_b0
-GROUP BY source,loss_type,trt,region);
+GROUP BY source,loss_type,region);
 
 -- create temp table for agg
-CREATE TABLE psra_{prov}.psra_{prov}_src_loss_r2_temp AS
+CREATE TABLE psra_{prov}.psra_{prov}_src_loss_r1_temp AS
 (
 SELECT
 source,
 loss_type,
-trt,
 SUM(loss_value) AS "loss_value",
 region
 
 
-FROM psra_{prov}.psra_{prov}_src_loss_r2
-GROUP BY source,loss_type,trt,region);
+FROM psra_{prov}.psra_{prov}_src_loss_r1
+GROUP BY source,loss_type,region);
 
 -- combine src tables into one
 CREATE TABLE psra_{prov}.psra_{prov}_src_loss AS
 (
 SELECT a.source,
 a.loss_type,
-a.trt,
 a.region,
 a.loss_value AS "loss_value_b0",
-b.loss_value AS "loss_value_r2"
+b.loss_value AS "loss_value_r1"
 
 FROM psra_{prov}.psra_{prov}_src_loss_b0_temp a
-INNER JOIN psra_{prov}.psra_{prov}_src_loss_r2_temp b ON a.source = b.source AND a.loss_type = b.loss_type AND a.trt = b.trt AND a.region = b.region
+INNER JOIN psra_{prov}.psra_{prov}_src_loss_r1_temp b ON a.source = b.source AND a.loss_type = b.loss_type AND a.region = b.region
 );
 
-DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_src_loss_b0_temp, psra_{prov}.psra_{prov}_src_loss_r2_temp CASCADE;
+DROP TABLE IF EXISTS psra_{prov}.psra_{prov}_src_loss_b0_temp, psra_{prov}.psra_{prov}_src_loss_r1_temp CASCADE;
