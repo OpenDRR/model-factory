ed9021d7d1191d6652acf3703cba014ecd2b3e93
William Chow <59062243+wkhchow@users.noreply.github.com>
2021-10-07 16:49:26 -0700

    Update Create_MH_risk_sauid_prioritization_Canada.sql
    - changed all_indicators to indicators

--- scripts/Create_MH_risk_sauid_prioritization_Canada.sql
+++ scripts/Create_MH_risk_sauid_prioritization_Canada.sql
@@ -1,8 +1,8 @@
 -- create schema for new scenario
 CREATE SCHEMA IF NOT EXISTS results_nhsl_hazard_threat;
 
-DROP TABLE IF EXISTS results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s_tbl CASCADE;
-CREATE TABLE results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s_tbl AS
+DROP TABLE IF EXISTS results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s_tbl CASCADE;
+CREATE TABLE results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s_tbl AS
 SELECT
 -- location
 a."Sauid",
@@ -1058,23 +1058,23 @@ a.geom_poly
 
 FROM results_nhsl_hazard_threat.hazard_threat_values_s a
 LEFT JOIN results_nhsl_hazard_threat.hazard_threat_values_csd b ON a.csduid = b.csduid
-LEFT JOIN results_nhsl_social_fabric.nhsl_social_fabric_all_indicators_s_tbl c ON a."Sauid" = c."Sauid"
+LEFT JOIN results_nhsl_social_fabric.nhsl_social_fabric_indicators_s_tbl c ON a."Sauid" = c."Sauid"
 ORDER BY a."Sauid" ASC; 
 
 -- update total absolute and relative scores
-UPDATE results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s_tbl
+UPDATE results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s_tbl
 SET total_abs_score = eq_shaking_score_abs + fld_priority_score_abs + wildfire_priority_score_abs + cy_priority_score_abs;
 
-UPDATE results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s_tbl
+UPDATE results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s_tbl
 SET total_rel_score = eq_shaking_score_rel + fld_priority_score_rel + wildfire_priority_score_rel + cy_priority_score_rel;
 
 
-
 -- create view
-DROP VIEW IF EXISTS results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s CASCADE;
+DROP VIEW IF EXISTS results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s CASCADE;
 
-CREATE VIEW results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s AS 
-SELECT * FROM results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s_tbl;
+CREATE VIEW results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s AS 
+SELECT * FROM results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s_tbl;
+
 
 -- aggregate to csd level
 DROP TABLE IF EXISTS results_nhsl_hazard_threat.nhsl_hazard_threat_prioritization_csd_tbl CASCADE;
@@ -1098,7 +1098,7 @@ SUM(cy_priority_score_rel) AS "cy_priority_score_rel",
 SUM(total_abs_score) AS "total_abs_score",
 SUM(total_rel_score) AS "total_rel_score",
 b.geom
-FROM results_nhsl_hazard_threat.nhsl_hazard_threat_all_indicators_s_tbl a
+FROM results_nhsl_hazard_threat.nhsl_hazard_threat_indicators_s_tbl a
 LEFT JOIN boundaries."Geometry_CSDUID" b on a.csduid = b."CSDUID"
 GROUP BY a.csduid,b."CSDNAME",b.geom;
 
