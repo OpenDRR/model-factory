44845e1b55cc88c600e9c59c00cd0f71753ed037
drotheram <drew.rotheram-clarke@canada.ca>
2021-11-08 16:27:31 -0800

    fix economic region parsing bug

--- scripts/PSRA_combineAggLossesStats.py
+++ scripts/PSRA_combineAggLossesStats.py
@@ -34,16 +34,19 @@ def main():
 
         for erFile in erFileList:
             dfFinal = pd.read_csv(erFile)
-            er = erFile.split('_')[1]
+            # er = erFile.split('_')[1]
             # Remove the split econmic region identifiers
             # handle subregions and combined regions differently
             # For example 'QC2445-55' should remain the same
             # NB1330 should remain the same
             # BC5920A2 should be changed to BC5920
-            if len(re.split('(\d+)', er)) == 1 or re.split('(\d+)', er)[2] == '-':
-                er = ''.join(re.split('(\d+)', er)[0:4])
-            else:
-                er = ''.join(re.split('(\d+)', er)[0:2])
+            er = erFile.split('ebR_')[1].split('_agg_losses-stats_{}.csv'.format(retrofit))[0]
+            # 2021-11-08 Updated, er will now be any of the text which the OpenQuake Analyst
+            # chooses to put in the filename between 'ebR_' and '_agg_curves-stats_{}.csv
+            # if len(re.split('(\d+)', er)) == 1 or re.split('(\d+)', er)[2] == '-':
+            #     er = ''.join(re.split('(\d+)', er)[0:4])
+            # else:
+            #     er = ''.join(re.split('(\d+)', er)[0:2])
 
             dfFinal['region'] = er
             os.rename(erFile, '{}_orginal.csv'.format(os.path.splitext(erFile)[0]))
