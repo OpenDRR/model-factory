96945e5cb097efe30a4c8b6cf0975ac6b09be75d
William Chow <59062243+wkhchow@users.noreply.github.com>
2022-01-26 15:36:07 -0800

    add shakemap hexbins aggregation for 5,10,25,50,100km

--- scripts/Create_scenario_risk_building_Indicators_ALL_shkmp.sql
+++ scripts/Create_scenario_risk_building_Indicators_ALL_shkmp.sql
@@ -56,7 +56,8 @@ DROP VIEW IF EXISTS  results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap;
 CREATE VIEW results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap AS SELECT * FROM results_dsra_{eqScenario}.{eqScenario}_shakemap_tbl;
 
 
--- create shakemap in hexbin for display
+
+-- create shakemap in hexbin for display - 5km
 DROP VIEW IF EXISTS results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_5km CASCADE;
 CREATE VIEW results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_5km AS
 
@@ -70,6 +71,67 @@ b.geom
 FROM results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap a
 JOIN boundaries."HexGrid_5km" b ON ST_INTERSECTS(a.geom,b.geom)
 GROUP BY b.gridid_5;
+
+-- create shakemap in hexbin for display - 10km
+DROP VIEW IF EXISTS results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_10km CASCADE;
+CREATE VIEW results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_10km AS
+
+SELECT
+b.gridid_10,
+AVG("sH_PGA") AS "sH_PGA_avg",
+MIN("sH_PGA") as "sH_PGA_min",
+MAX("sH_PGA") as "sH_PGA_max",
+b.geom
+
+FROM results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap a
+JOIN boundaries."HexGrid_10km" b ON ST_INTERSECTS(a.geom,b.geom)
+GROUP BY b.gridid_10;
+
+-- create shakemap in hexbin for display - 25km
+DROP VIEW IF EXISTS results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_25km CASCADE;
+CREATE VIEW results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_25km AS
+
+SELECT
+b.gridid_25,
+AVG("sH_PGA") AS "sH_PGA_avg",
+MIN("sH_PGA") as "sH_PGA_min",
+MAX("sH_PGA") as "sH_PGA_max",
+b.geom
+
+FROM results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap a
+JOIN boundaries."HexGrid_25km" b ON ST_INTERSECTS(a.geom,b.geom)
+GROUP BY b.gridid_25;
+
+-- create shakemap in hexbin for display - 50km
+DROP VIEW IF EXISTS results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_50km CASCADE;
+CREATE VIEW results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_50km AS
+
+SELECT
+b.gridid_50,
+AVG("sH_PGA") AS "sH_PGA_avg",
+MIN("sH_PGA") as "sH_PGA_min",
+MAX("sH_PGA") as "sH_PGA_max",
+b.geom
+
+FROM results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap a
+JOIN boundaries."HexGrid_50km" b ON ST_INTERSECTS(a.geom,b.geom)
+GROUP BY b.gridid_50;
+
+-- create shakemap in hexbin for display - 100km
+DROP VIEW IF EXISTS results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_100km CASCADE;
+CREATE VIEW results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap_hexbin_100km AS
+
+SELECT
+b.gridid_100,
+AVG("sH_PGA") AS "sH_PGA_avg",
+MIN("sH_PGA") as "sH_PGA_min",
+MAX("sH_PGA") as "sH_PGA_max",
+b.geom
+
+FROM results_dsra_{eqScenario}.dsra_{eqScenario}_shakemap a
+JOIN boundaries."HexGrid_100km" b ON ST_INTERSECTS(a.geom,b.geom)
+GROUP BY b.gridid_100;
+
 
 
 -- add polygon extents to scenario extents table for each scenario
