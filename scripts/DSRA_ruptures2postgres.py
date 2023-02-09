OpenDRR/model-factory

commit 6978357aa2725f452667525c4fc571e8ff055483

fix rupture parsing error

scripts/DSRA_ruptures2postgres.py
@@ -54,19 +54,14 @@ def main():
         # logging.info(response.content)
         response.raise_for_status()
         repo_dict = json.loads(response.content)
-        repo_list = []
 
         for item in repo_dict:
-            if (item['type'] == 'file' and
-                    'rupture' in item['name'] and
-                    'xml' in item['name']):
-                repo_list.append(item['name'])
-        logging.info(repo_list)
+            logging.info(item['name'])
     except requests.exceptions.RequestException as e:
         logging.error(e)
         sys.exit()
 
-    processRuptureXML(repo_list, engine, auth, url, args)
+    processRuptureXML(repo_dict, engine, auth)
 
     return
 
@@ -96,12 +91,10 @@ def parse_args():
     return args
 
 
-def processRuptureXML(repo_list, engine, auth, url, args):
-    for ruptureFile in repo_list:
-        logging.info("processing: {}".format(ruptureFile))
-        item_url = (f'https://raw.githubusercontent.com/'
-                    f'{args.dsraRuptureRepo}/'
-                    f'{args.dsraRuptureBranch}/{ruptureFile}')
+def processRuptureXML(repo_dict, engine, auth):
+    for ruptureFile in repo_dict:
+        logging.info("processing: {}".format(ruptureFile['name']))
+        item_url = ruptureFile['download_url']
         ghToken = auth.get('auth', 'github_token')
         response = requests.get(item_url,
                                 headers={'Authorization': f'token {ghToken}'})
@@ -116,7 +109,7 @@ def processRuptureXML(repo_list, engine, auth, url, args):
         xroot = et.ElementTree(et.fromstring(response.content)).getroot()
         xmlns = '{http://openquake.org/xmlns/nrml/0.4}'
         source_type = xroot[0].tag.replace(f'{xmlns}', '')
-        rupture_name = os.path.splitext(ruptureFile)[0].replace('rupture_', '')
+        rupture_name = os.path.splitext(ruptureFile['name'])[0].replace('rupture_', '')
         magnitude = float(xroot[0].find(f"{xmlns}magnitude").text)
         rake = xroot[0].find(f"{xmlns}rake").text
         lon = xroot[0].find(f"{xmlns}hypocenter").attrib.get('lon')

commit 56ca527188b3b650e68eafc1d472c83f4f65bc2d

DSRA_ruptures2postgres.py: Process only rupture_*.xml files as before
This avoids unintended errors when non-XML files (e.g. CSV files) are
encountered.

Thanks to William Chow (@wkhchow) for discovering the issue.

Fixes a regression in commit 6978357aa2725f452667525c4fc571e8ff055483.

scripts/DSRA_ruptures2postgres.py
@@ -16,6 +16,7 @@ import csv
 import json
 import logging
 import os
+import re
 import sys
 import xml.etree.ElementTree as et
 from io import StringIO
@@ -103,6 +104,12 @@ def parse_args():
 
 def process_rupture_xml(repo_dict, engine, auth):
     for rupture_file in repo_dict:
+        if not (
+            rupture_file["type"] == "file"
+            and re.fullmatch(r"rupture_.*\.xml", rupture_file["name"])
+        ):
+            logging.warning("skipping: %s", rupture_file["name"])
+            continue
         logging.info("processing: %s", rupture_file["name"])
         item_url = rupture_file["download_url"]
         gh_token = auth.get("auth", "github_token")